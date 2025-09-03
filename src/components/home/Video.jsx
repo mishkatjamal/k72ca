7e6b892e6206caae2be7789ca35e03a46e0629d6
mishkatjamal <mishkatjamal13@gmail.com>
2025-09-03 06:24:49 +0530

    Fixed video path for Render deployment

--- src/components/home/Video.jsx
+++ src/components/home/Video.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const video = () => {
   return (
     <div className='h-full w-full'>
-        <video autoPlay loop muted className='h-full w-full object-cover' src="/public/83c745cf.mp4"></video>
+        <video autoPlay loop muted className='h-full w-full object-cover' src="/83c745cf.mp4"></video>
         
     </div>
   )
